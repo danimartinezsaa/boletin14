8126d731978f1d36aae88fdd796e28c40e9cf147
dmartinezsaa <dani@dani-GE60-2QD>
2017-12-11 19:12:07 +0100

    v1.0

--- src/boletin14/Boletin14.java
+++ src/boletin14/Boletin14.java
@@ -19,60 +19,15 @@ public class Boletin14{
     public static void main(String[] args){
         String[] opciones={"Añadir","Retirar","Salir"};
         Garaje garaje=new Garaje();
-        Coche coches[]=new Coche[6];
-        
-        coches[0]=new Coche();
-        coches[1]=new Coche();
-        coches[2]=new Coche();
-        coches[3]=new Coche();
-        coches[4]=new Coche();
-        coches[5]=new Coche();
-        
-        
+           
         while (true){
-            
             int opcion=JOptionPane.showOptionDialog(null, "Añada o retire coche", "Garage", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opciones, null);
             if(opcion==0){
-                boolean b=false;
-                
-                try{
-                    for(byte i=0;i<garaje.getPlazas();i++){
-                            if(coches[i].getMatricula()==null){
-                                String a=JOptionPane.showInputDialog("Inserte matrícula:");
-                                coches[i].setMatricula(a);
-                                garaje.añadirCoche();
-                                b=true;
-                                break;
-                                }    
-                    }
-                }catch(Exception ex1){
-                            JOptionPane.showMessageDialog(null,"Matrícula existente.");
-                            break;
-                            }
-                if(b==false) JOptionPane.showMessageDialog(null, "Garage Lleno, espere.");
-                else JOptionPane.showMessageDialog(null, "Coche dentro!");
-                
+                garaje.añadirCoche();
             }else if (opcion==1){
-                boolean c=false;
-                String b=JOptionPane.showInputDialog("Inserte matrícula: ");
-                for(byte i=0;i<=garaje.getPlazas();i++){
-                    try{
-                        if(coches[i].getMatricula().equals(b)){
-                            coches[i].setMatricula(null);
-                            garaje.retirarCoche();
-                            c=true;
-                            break;
-                        }
-                    }catch(Exception ex2){
-                        break;
-                    }
-                }
-                
-                if(c==false) JOptionPane.showMessageDialog(null, "Matrícula no encontrada.");
-                else JOptionPane.showMessageDialog(null, "Coche fuera!");
-            } else break;
+                garaje.retirarCoche();
+            }else break;
         
         }
     }
-    
 }
--- src/boletin14/Garaje.java
+++ src/boletin14/Garaje.java
@@ -12,8 +12,12 @@ import javax.swing.JOptionPane;
  * @author dani
  */
 public class Garaje{
-    private int numeroCoches;
+    private int numeroCoches=0;
     private final int plazas=5;
+    byte inicio=0;
+    
+    Coche coches[]=new Coche[6];
+        
 
     //Constructor
     public Garaje(){
@@ -43,15 +47,50 @@ public class Garaje{
     
     //program
     public void añadirCoche(){
-        if(numeroCoches>=plazas) {
-            JOptionPane.showMessageDialog(null, "Garage Lleno, no puede entrar.");
-        } else numeroCoches++;
+        if(inicio==0){
+            coches[0]=new Coche();
+            coches[1]=new Coche();
+            coches[2]=new Coche();
+            coches[3]=new Coche();
+            coches[4]=new Coche();
+            coches[5]=new Coche();
+            inicio=1;
+        }
+
+        if(numeroCoches<plazas){
+            for(byte i=0;i<plazas;i++){
+                if(coches[i].getMatricula()==null){
+                    String a=JOptionPane.showInputDialog("Inserte matrícula:");
+                    coches[i].setMatricula(a);
+                    numeroCoches++;
+                    JOptionPane.showMessageDialog(null, "Coche dentro!");
+                    break;
+                }
+            }
+        }else if(numeroCoches==plazas) {
+            JOptionPane.showMessageDialog(null, "Garaje Lleno, no puede entrar.");
+        }
         
     }
     
     public void retirarCoche(){
+
         if(numeroCoches==0){
             JOptionPane.showMessageDialog(null, "No hay coches que retirar");
-        } else numeroCoches--;
+        } else {
+            String b=JOptionPane.showInputDialog("Inserte matrícula: ");
+            for(byte i=0;i<plazas;i++){
+                try{
+                    if(coches[i].getMatricula().equals(b)){
+                    coches[i].setMatricula(null);
+                    numeroCoches=numeroCoches-1;
+                    JOptionPane.showMessageDialog(null, "Coche fuera!");
+                    break;
+                    }
+                }catch(Exception ex2){
+                    break;
+                }
+            }  
+        }
     }
 }
